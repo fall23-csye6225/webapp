e0e3f03cb70800d52b7fdba438f37d3e55a66d8e
srushtyreturi <114314379+srushtyreturi@users.noreply.github.com>
2023-10-25 00:21:37 -0400

    Removed db setup (#12)
    Co-authored-by: SrushtyR <srushtyr@Srushtys-MacBook-Air.local>

--- setup.sh
+++ setup.sh
@@ -27,24 +27,24 @@ sudo apt-get install -y npm
 # Display npm version
 npm -v
 
-# Install MariaDB server
-sudo apt-get install -y mariadb-server
+# # Install MariaDB server
+# sudo apt-get install -y mariadb-server
 
-# Define your MySQL root password
-#DB_HOST=127.0.0.1
-DB_USER=root
-DB_PASSWORD=root
-DB_NAME=assignmentsdb
+# # Define your MySQL root password
+# #DB_HOST=127.0.0.1
+# DB_USER=root
+# DB_PASSWORD=root
+# DB_NAME=assignmentsdb
 
-sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$DB_PASSWORD');FLUSH PRIVILEGES;"
+# sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('$DB_PASSWORD');FLUSH PRIVILEGES;"
 
-printf "%s\n n\n n\n n\n n\n n\n y\n" "$DB_PASSWORD" | sudo mysql_secure_installation
+# printf "%s\n n\n n\n n\n n\n n\n y\n" "$DB_PASSWORD" | sudo mysql_secure_installation
 
-sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+# sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 
-mysql -u root -p"$DB_PASSWORD" -Bse "CREATE DATABASE $DB_NAME;"
+# mysql -u root -p"$DB_PASSWORD" -Bse "CREATE DATABASE $DB_NAME;"
 
-mysql -u root -p"$DB_PASSWORD" -Bse "SHOW DATABASES;"
+# mysql -u root -p"$DB_PASSWORD" -Bse "SHOW DATABASES;"
 
 # Clean up
 sudo apt-get clean
